e44686e7f8f348a27f383b8fb5d10fd7fabbb5bf
Will Anderson <williamv.anderson@gmail.com>
2013-06-08 16:53:50 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Txt2cookEngine::Application.config.secret_token = 'db25cbf5a9a4a4d8aa862038c59269d89ca259a1ea10ec589110e65eedb7318e57e8a71fd14f2ef747c75b1417a2013c27ce4a86f84a347f127dd2fd9c7dc5b6'

6c45060c08d9b64d50e44d86ca8be3695613574a
Will Anderson <williamv.anderson@gmail.com>
2013-06-08 17:47:27 -0400

    another model validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ActiveRecord::Base
 	attr_accessible :recipe_name, :difficulty_level, :total_time, :ingredient_count
 	validates :recipe_name, :difficulty_level, :total_time, :ingredient_count, presence: true
+	validates :total_time,:ingredient_count, :numericality => {:only_integer => true}
 end
--- test/unit/recipe_test.rb
+++ test/unit/recipe_test.rb
@@ -5,24 +5,24 @@ class RecipeTest < ActiveSupport::TestCase
 		r = Recipe.new 
 		r.recipe_name = "Orzo"
 		r.difficulty_level = "Easy"
-		r.total_time = "15 minutes"
-		r.ingredient_count = "20"		
+		r.total_time = 15
+		r.ingredient_count = 5		
 		assert r.save, "Cannot create a valid recipe"
 	end
 
 	test "shouldn't be able to create a new recipe without recipe name" do
 		r = Recipe.new
 		r.difficulty_level = "Easy"
-		r.total_time = "15 minutes"
-		r.ingredient_count = "20"		
+		r.total_time = 15
+		r.ingredient_count = 5		
 		assert !r.save, "Prevent recipe creation without a recipe name"
 	end
 
 	test "shouldn't be able to create a new recipe without a difficulty level" do
 		r = Recipe.new
 		r.recipe_name = "Orzo"
-		r.total_time = "15 minutes"
-		r.ingredient_count = "20"		
+		r.total_time = 15
+		r.ingredient_count = 5	
 		assert !r.save, "Prevent recipe creating if difficulty level is missing"
 	end
 
@@ -30,7 +30,7 @@ class RecipeTest < ActiveSupport::TestCase
 		r = Recipe.new
 		r.recipe_name = "Orzo"
 		r.difficulty_level = "Easy"
-		r.ingredient_count = "20"
+		r.ingredient_count = 20
 		assert !r.save, "Prevent recipe creation if total_time is missing"
 	end
 
@@ -38,8 +38,17 @@ class RecipeTest < ActiveSupport::TestCase
 		r = Recipe.new
 		r.recipe_name = "Orzo"
 		r.difficulty_level = "Easy"
-		r.total_time = "15 minutes"
+		r.total_time =  15
 		assert !r.save, "Prevent recipe creation if ingredient count is missing"
+	end
+
+	test "each field should only accept well formed data for it's type" do
+		r = Recipe.new
+		r.recipe_name = 123
+		r.difficulty_level = 123
+		r.total_time = "Here's a string"
+		r.ingredient_count = "Here's another string"
+		assert !r.save, "Shouldn't be able to save a recipe with bad data"
 	end
 end
 
